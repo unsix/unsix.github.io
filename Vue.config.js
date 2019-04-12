d9d25227bf8bf0f8b8328459c6b5e5f61766fa4f
unsix <UnWelt@qq.com>
2019-04-12 20:05:53 +0800

    配置

--- Vue.config.js
+++ Vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/vue-github/'
-    : '/',
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
+}
